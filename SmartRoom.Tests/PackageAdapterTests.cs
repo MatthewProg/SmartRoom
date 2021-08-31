cd4db7405002c9caa8d795f6910042e6c4b43a8e
Mateusz Ciaglo <mat.ciaglo@gmail.com>
2021-08-31 14:49:17 +0200

    Add PackageAdapter and create tests for it
    Added:
    - PackageAdapter CreateSetPackage() & CreateGetPackage()
    - PackageAdapter tests for new methods

---
+++ SmartRoom.Tests/PackageAdapterTests.cs
@@ -0,0 +1,130 @@
+﻿using NUnit.Framework;
+using SmartRoom.Adapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.UITest;
+
+namespace SmartRoom.Tests
+{
+    [TestFixture(Platform.Android)]
+    public class PackageAdapterTests
+    {
+        IApp app;
+        Platform platform;
+
+        public PackageAdapterTests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void Init()
+        {
+            PackageAdapter.AnalogPinsOffset = 14U;
+        }
+
+        [TestCase("0", false, 128, new byte[2] { 0b11000000, 128 })]
+        [TestCase("0", true, 98, new byte[2] { 0b11100000, 98 })]
+        [TestCase("6", false, 56, new byte[2] { 0b11000110, 56 })]
+        [TestCase("6", true, 12, new byte[2] { 0b11100110, 12 })]
+        [TestCase("006", false, 56, new byte[2] { 0b11000110, 56 })]
+        [TestCase("006", true, 12, new byte[2] { 0b11100110, 12 })]
+        [TestCase("31", false, 0, new byte[2] { 0b11011111, 0 })]
+        [TestCase("31", true, 255, new byte[2] { 0b11111111, 255 })]
+        [TestCase("A0", false, 9, new byte[2] { 0b10001110, 9 })]
+        [TestCase("a0", true, 78, new byte[2] { 0b10101110, 78 })]
+        [TestCase("A5", false, 90, new byte[2] { 0b10010011, 90 })]
+        [TestCase("a5", true, 66, new byte[2] { 0b10110011, 66 })]
+        [TestCase("D10", false, 1, new byte[2] { 0b10001010, 1 })]
+        [TestCase("d10", true, 32, new byte[2] { 0b10101010, 32 })]
+        [TestCase("D000010", false, 1, new byte[2] { 0b10001010, 1 })]
+        [TestCase("d000010", true, 32, new byte[2] { 0b10101010, 32 })]
+        public void CreateSetPackageCorrect(string pin, bool fade, byte value, byte[] expected)
+        {
+            var result = PackageAdapter.CreateSetPackage(pin, fade, value);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("  ")]
+        public void CreateSetPackageNullPin(string pin)
+        {
+            Assert.Throws<ArgumentNullException>(() => PackageAdapter.CreateSetPackage(pin, false, 255));
+        }
+
+        [TestCase("c1")]
+        [TestCase("-9")]
+        [TestCase("D-1")]
+        [TestCase("0.5")]
+        [TestCase("0,5")]
+        [TestCase("D")]
+        [TestCase("a")]
+        public void CreateSetPackageWrongPin(string pin)
+        {
+            Assert.Throws<ArgumentException>(() => PackageAdapter.CreateSetPackage(pin, false, 255));
+        }
+
+        [TestCase("32")]
+        [TestCase("D40")]
+        [TestCase("a33")]
+        public void CreateSetPackageOverflowPin(string pin)
+        {
+            Assert.Throws<OverflowException>(() => PackageAdapter.CreateSetPackage(pin, false, 255));
+        }
+
+        [TestCase("0", false, 0b00100000)]
+        [TestCase("0", true, 0b01000000)]
+        [TestCase("d0", false, 0b00000000)]
+        [TestCase("A0", false, 0b00001110)]
+        [TestCase("10", false, 0b00101010)]
+        [TestCase("010", false, 0b00101010)]
+        [TestCase("10", true, 0b01001010)]
+        [TestCase("0010", true, 0b01001010)]
+        [TestCase("d10", false, 0b00001010)]
+        [TestCase("d00010", false, 0b00001010)]
+        [TestCase("A5", false, 0b00010011)]
+        [TestCase("A05", false, 0b00010011)]
+        public void CreateGetPackageCorrect(string pinId, bool isId, byte expected)
+        {
+            var result = PackageAdapter.CreateGetPackage(pinId, isId);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("  ")]
+        public void CreateGetPackageNullPin(string pin)
+        {
+            Assert.Throws<ArgumentNullException>(() => PackageAdapter.CreateGetPackage(pin));
+        }
+
+        [TestCase("c1", false)]
+        [TestCase("-9", false)]
+        [TestCase("D-1", false)]
+        [TestCase("0.5", false)]
+        [TestCase("0,5", false)]
+        [TestCase("D", false)]
+        [TestCase("a", false)]
+        [TestCase("a1", true)]
+        [TestCase("D10", true)]
+        public void CreateGetPackageWrongPin(string pin, bool isId)
+        {
+            Assert.Throws<ArgumentException>(() => PackageAdapter.CreateGetPackage(pin, isId));
+        }
+
+        [TestCase("32", false)]
+        [TestCase("37", true)]
+        [TestCase("D40", false)]
+        [TestCase("a33", false)]
+        public void CreateGetPackageOverflowPin(string pin, bool isId)
+        {
+            Assert.Throws<OverflowException>(() => PackageAdapter.CreateGetPackage(pin, isId));
+        }
+    }
+}
---
+++ SmartRoom/Adapters/PackageAdapter.cs
@@ -0,0 +1,94 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmartRoom.Adapters
+{
+    public static class PackageAdapter
+    {
+        public static UInt32 AnalogPinsOffset { get; set; } = 14U;
+
+        public static byte[] CreateSetPackage(string pin, bool fade, byte value)
+        {
+            byte header = (byte)(fade ? 0b10100000 : 0b10000000);
+
+            if (string.IsNullOrWhiteSpace(pin)) 
+                throw new ArgumentNullException("pin", "Pin cannot be null or whitespace");
+            pin = pin.Trim().ToUpper();
+
+            //Set destination
+            byte pinByte = 0;
+            if(pin[0] == 'A' || pin[0] == 'D')
+            {
+                if (pin.Length == 1) 
+                    throw new ArgumentException("Pin number is incorrect", "pin");
+                if (byte.TryParse(pin.Substring(1), out pinByte) == false) 
+                    throw new ArgumentException("Pin number is incorrect or too big", "pin");
+
+                if (pin[0] == 'A') pinByte += (byte)AnalogPinsOffset;
+            }
+            else
+            {
+                if (byte.TryParse(pin, out pinByte) == false) 
+                    throw new ArgumentException("Pin number is incorrect or too big", "pin");
+                header |= 0b01000000;
+            }
+
+            //Set pin
+            if (pinByte > 0b00011111)
+                throw new OverflowException("Pin number is too big");
+            header += pinByte;
+
+            return new byte[2] { header, value };
+        }
+
+        public static byte CreateGetPackage(string pinId, bool isId = false)
+        {
+            byte header = (byte)(isId ? 0b01000000 : 0b00000000);
+
+            if (string.IsNullOrWhiteSpace(pinId))
+                throw new ArgumentNullException("pinId", "Pin/ID cannot be null or whitespace");
+            pinId = pinId.Trim().ToUpper();
+
+            //Set destination
+            byte pinByte = 0;
+            if (isId == false)
+            {
+                if (pinId[0] == 'A' || pinId[0] == 'D')
+                {
+                    if (pinId.Length == 1)
+                        throw new ArgumentException("Pin/ID number is incorrect", "pinId");
+                    if (byte.TryParse(pinId.Substring(1), out pinByte) == false)
+                        throw new ArgumentException("Pin/ID number is incorrect or too big", "pinId");
+
+                    if (pinId[0] == 'A') pinByte += (byte)AnalogPinsOffset;
+                }
+                else
+                {
+                    if (byte.TryParse(pinId, out pinByte) == false)
+                        throw new ArgumentException("Pin/ID number is incorrect or too big", "pinId");
+                    header |= 0b00100000;
+                }
+            }
+            else
+            {
+                if (byte.TryParse(pinId, out pinByte) == false)
+                    throw new ArgumentException("Pin/ID number is incorrect or too big", "pinId");
+            }
+
+            //Set pin
+            if (pinByte > 0b00011111)
+                throw new OverflowException("Pin number is too big");
+            header += pinByte;
+
+            return header;
+        }
+    }
+}
