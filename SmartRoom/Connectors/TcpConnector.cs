849f0b9b490e29e39976f271e4c8490add74d290
Mateusz Ciaglo <mat.ciaglo@gmail.com>
2021-09-01 18:29:14 +0200

    Add TcpConnector and PackagesManager
    Added:
    - TcpConnector class
    - PackagesManager class

---
+++ SmartRoom/Connectors/TcpConnector.cs
@@ -0,0 +1,108 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartRoom.Connectors
+{
+    public class TcpConnector
+    {
+        private const int SERVER_BUFFER = 32;
+
+        private TcpClient _client;
+        private string _address;
+        private int _port;
+
+        private List<byte> _sendBuffer;
+        private List<byte> _receiveBuffer;
+        private Task _processTask;
+
+        public event EventHandler DataReceivedEvent;
+
+        public TcpConnector(string address, int port)
+        {
+            _address = address;
+            _port = port;
+
+            _sendBuffer = new List<byte>();
+            _receiveBuffer = new List<byte>();
+
+            _client = new TcpClient();
+        }
+
+        public void Connect() => Connect(_address, _port);
+        public void Connect(string address, int port)
+        {
+            if (IsConnected)
+                Close();
+
+            Task.Run(async () => await _client.ConnectAsync(address, port));
+        }
+
+        public bool IsConnected => _client?.Connected ?? false;
+        public bool IsReady => (_processTask == null || _processTask.IsCompleted == true);
+
+        public void Send(byte[] data)
+        {
+            _sendBuffer.AddRange(data);
+            if (IsReady)
+                _processTask = Task.Run(async () => { await Process(); DataReceivedEvent?.Invoke(_receiveBuffer, null); });
+            else
+                _processTask.ContinueWith(delegate {
+                    _processTask = Task.Run(async () => { await Process(); DataReceivedEvent?.Invoke(_receiveBuffer, null); });
+                });
+        }
+
+        private async Task Process()
+        {
+            if(IsConnected)
+            {
+                var stream = _client.GetStream();
+                while (_sendBuffer.Count != 0)
+                {
+                    var range = (_sendBuffer.Count < SERVER_BUFFER ? _sendBuffer.Count : SERVER_BUFFER);
+                    var toSend = _sendBuffer.GetRange(0, range);
+                    _sendBuffer.RemoveRange(0, range);
+
+                    await stream.WriteAsync(toSend.ToArray(), 0, toSend.Count);
+
+                    while (true)
+                    {
+                        try
+                        {
+                            int lastRead = stream.ReadByte();
+                            if (lastRead == -1) break;
+
+                            _receiveBuffer.Add((byte)lastRead);
+                        }
+                        catch (TimeoutException) { ; }
+                    }
+                }
+            }
+        }
+
+        public void Close()
+        {
+            if (IsConnected)
+            {
+                if (_processTask != null)
+                    _processTask.Dispose();
+                _client.Close();
+            }
+        }
+
+        ~TcpConnector()
+        {
+            Close();
+        }
+    }
+}
---
+++ SmartRoom/Managers/PackagesManager.cs
@@ -0,0 +1,152 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SmartRoom.Connectors;
+using SmartRoom.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace SmartRoom.Managers
+{
+    public class PackagesManager
+    {
+        public TcpConnector Connection { get; private set; }
+
+        private ObservableCollection<Models.SwitchModel> _switches;
+        private Dictionary<string, Tuple<bool, byte>> _pinValue;
+        private Models.SettingsModel _settings;
+
+        public PackagesManager(ObservableCollection<SwitchModel> switches, SettingsModel settings)
+        {
+            _switches = switches;
+            _settings = settings;
+            _pinValue = new Dictionary<string, Tuple<bool, byte>>();
+
+            _switches.CollectionChanged += SwitchesCollectionChanged;
+
+            Connection = new TcpConnector(_settings.Address, _settings.Port);
+            Connection.DataReceivedEvent += DataReceived;
+        }
+
+        private void SwitchesCollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if (e.OldItems != null)
+            {
+                foreach (Models.SwitchModel m in e.OldItems)
+                    m.PropertyChanged -= SwitchPropertyChanged;
+            }
+            if (e.NewItems != null)
+            {
+                foreach (Models.SwitchModel m in e.NewItems)
+                    m.PropertyChanged += SwitchPropertyChanged;
+            }
+        }
+
+        private void SwitchPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Toggle" || e.PropertyName == "Value" || e.PropertyName == "Color")
+            {
+                if (sender is Models.ToggleSwitchModel)
+                {
+                    var model = sender as Models.ToggleSwitchModel;
+                    _pinValue[model.Pin] = new Tuple<bool, byte>(model.Fade, (byte)(model.Toggle ? 255 : 0));
+                }
+                else if (sender is Models.SliderSwitchModel)
+                {
+                    var model = sender as Models.SliderSwitchModel;
+                    _pinValue[model.Pin] = new Tuple<bool, byte>(model.Fade, (byte)Math.Round(model.Value * 255));
+                }
+                else if (sender is Models.ColorSwitchModel)
+                {
+                    var model = sender as Models.ColorSwitchModel;
+                    var col = model.Color.GetRGB();
+
+                    _pinValue[model.RedPin] =   new Tuple<bool, byte>(model.Fade, col.R);
+                    _pinValue[model.GreenPin] = new Tuple<bool, byte>(model.Fade, col.G);
+                    _pinValue[model.BluePin] =  new Tuple<bool, byte>(model.Fade, col.B);
+                }
+
+                if (Connection.IsReady)
+                    SendWaiting();
+            }
+        }
+
+        private void DataReceived(object sender, EventArgs e)
+        {
+            var data = sender as List<byte>;
+            if (data.Count != 0)
+            {
+                var pkgs = Adapters.PackageAdapter.DeserializePackages(data.ToArray());
+                data.Clear();
+                ProcessPackages(pkgs);
+            }
+
+            if(Connection.IsConnected)
+                SendWaiting();
+        }
+
+        private void SendWaiting()
+        {
+            if (_pinValue.Count == 0)
+                return;
+
+            var data = new List<byte>();
+            foreach (var p in _pinValue)
+            {
+                data.AddRange(Adapters.PackageAdapter.CreateSetPackage(p.Key, p.Value.Item1, p.Value.Item2));
+                _pinValue.Remove(p.Key);
+            }
+            Connection.Send(data.ToArray());
+        }
+          
+        private void ProcessPackages(List<Models.PackageModel> packages) //Very intense process, try to optimize in future
+        {
+            foreach (var s in _switches)
+            {
+                if (s is Models.ToggleSwitchModel)
+                {
+                    var model = s as Models.ToggleSwitchModel;
+                    var pkg = packages.TakeWhile(x => x is Models.PackageValueModel)
+                                      .Select(x => x as Models.PackageValueModel)
+                                      .Where(x => x.PinId == model.Pin && x.IsID == false)
+                                      .FirstOrDefault();
+                    if (pkg == null) continue;
+                    model.Toggle = (pkg.Value != 0);
+                }
+                else if (s is Models.SliderSwitchModel)
+                {
+                    var model = s as Models.SliderSwitchModel;
+                    var pkg = packages.TakeWhile(x => x is Models.PackageValueModel)
+                                      .Select(x => x as Models.PackageValueModel)
+                                      .Where(x => x.PinId == model.Pin && x.IsID == false)
+                                      .FirstOrDefault();
+
+                    if (pkg == null) continue;
+                    model.Value = (float)pkg.Value / 255f;
+                }
+                else if(s is Models.ColorSwitchModel)
+                {
+                    var model = s as Models.ColorSwitchModel;
+                    var pkg = packages.TakeWhile(x => x is Models.PackageValueModel)
+                                      .Select(x => x as Models.PackageValueModel)
+                                      .Where(x => x.IsID == false && (x.PinId == model.RedPin || x.PinId == model.GreenPin || x.PinId == model.BluePin))
+                                      .ToList();
+
+                    if (pkg.Count == 0) continue;
+                    var color = model.Color.GetRGB();
+                    foreach (var p in pkg)
+                        if (p.PinId == model.RedPin) color.R = p.Value;
+                        else if (p.PinId == model.GreenPin) color.G = p.Value;
+                        else if (p.PinId == model.BluePin) color.B = p.Value;
+                    model.Color.FromRGB(color);
+                }
+            }
+        }
+    }
+}
